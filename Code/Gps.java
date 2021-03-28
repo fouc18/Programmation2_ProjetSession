78487097a13d64304d4a4752ec1d79f127f3711a
Gabriel Côté <cotg18@uqo.ca>
2021-03-28 17:25:31 -0400

    Create Gps

---
+++ Code/Gps.java
@@ -0,0 +1,25 @@
+package Code;
+
+public class Gps {
+	
+	int[] itineraire;
+	double distanceParcouru;
+	
+	public Gps() {
+		// TODO Auto-generated constructor stub
+		
+		
+	}
+	
+	public int[] getItineraire() {
+		
+		return this.itineraire;
+		
+	}
+	
+	public double getDistance() {
+		
+		return distanceParcouru;
+	}
+
+}
