3248a58e1169d1b46458b6ee85ddad094eb22f0e
Gabriel Côté <cotg18@uqo.ca>
2021-03-28 17:10:51 -0400

    Ajout de GPS

---
+++ Code/Gps.java
@@ -0,0 +1,33 @@
+package Code;
+
+public class Gps {
+
+	double distanceParcouru;
+	int [] itineraire;
+
+	public Gps() {
+
+
+		
+	}
+	
+
+	public int[] getItineraire() {
+
+		return this.itineraire;
+		
+	}
+
+	public double getDistance() {
+
+		return this.distanceParcouru;
+
+	}
+	
+	public void AjouterDistance(float distance) {
+		
+		
+		
+	}
+
+}
