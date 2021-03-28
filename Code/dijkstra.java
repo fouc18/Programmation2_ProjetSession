add1e93dbe854791615d46a402b7c888c66dd62e
CyberRenard <fouc18@uqo.ca>
2021-03-28 16:38:35 -0400

    dijkstra

--- Code/dijkstra.java
+++ Code/dijkstra.java
@@ -1,95 +1,151 @@
-// A Java program for Dijkstra's single source shortest path algorithm.
-// The program is for adjacency matrix representation of the graph
-import java.util.*;
-import java.lang.*;
-import java.io.*;
+package Code;// A Java program for Dijkstra's
+// single source shortest path
+// algorithm. The program is for
+// adjacency matrix representation
+// of the graph.
 
-class ShortestPath {
+class Dijkstra {
 
-    //Trouve la distance minimum entre le noeud actuel et les noeuds connexes qui n'ont pas été visité
-    static final int NB_NOEUD = 8;
-    int minDistance(int[] dist, Boolean[] cheminMin)
-    {
-        // Initialize min value
-        int min = Integer.MAX_VALUE, min_index = -1;
+    private static final int NO_PARENT = -1;
 
-        for (int v = 0; v < NB_NOEUD; v++)
-            if (!cheminMin[v] && dist[v] <= min) {
-                min = dist[v];
-                min_index = v;
-            }
-        System.out.println("min distance" + min_index);
-        return min_index;
-    }
-
-    // A utility function to print the constructed distance array
-    void printSolution(int[] dist)
-    {
-        System.out.println("Vertex \t\t Distance from Source");
-        for (int i = 0; i < NB_NOEUD; i++)
-            System.out.println(i + " \t\t " + dist[i]);
-    }
-
-    // Function that implements Dijkstra's single source shortest path
-    // algorithm for a graph represented using adjacency matrix
+    // Function that implements Dijkstra's
+    // single source shortest path
+    // algorithm for a graph represented
+    // using adjacency matrix
     // representation
-    void dijkstra(int[][] graph, int dep, int arv)
+    private static void dijkstra(int[][] adjacencyMatrix,
+                                 int startVertex)
     {
-        int[] dist = new int[NB_NOEUD]; // The output array. dist[i] will hold
-        // the shortest distance from dep to i
+        int nVertices = adjacencyMatrix[0].length;
 
-        //Création du tableau pour pouvoir track le chemin
-        int[] parents = new int[NB_NOEUD];
+        // shortestDistances[i] will hold the
+        // shortest distance from src to i
+        int[] shortestDistances = new int[nVertices];
 
-        //Le premier noeuds n'a aucun parent
-        parents[dep] = -1;
+        // added[i] will true if vertex i is
+        // included / in shortest path tree
+        // or shortest distance from src to
+        // i is finalized
+        boolean[] added = new boolean[nVertices];
 
-        // cheminMin[i] will true if vertex i is included in shortest
-        // path tree or shortest distance from dep to i is finalized
-        Boolean[] cheminMin = new Boolean[NB_NOEUD];
-
-        // Initialize all distances as INFINITE and stpSet[] as false
-        for (int i = 0; i < NB_NOEUD; i++) {
-            dist[i] = Integer.MAX_VALUE;
-            cheminMin[i] = false;
+        // Initialize all distances as
+        // INFINITE and added[] as false
+        for (int vertexIndex = 0; vertexIndex < nVertices;
+             vertexIndex++)
+        {
+            shortestDistances[vertexIndex] = Integer.MAX_VALUE;
+            added[vertexIndex] = false;
         }
 
-        // La distance du point de depart sera toujours zero
-        dist[dep] = 0;
+        // Distance of source vertex from
+        // itself is always 0
+        shortestDistances[startVertex] = 0;
 
-        // trouve le plus court chemin pour chaque noeuds
-        for (int count = 0; count < NB_NOEUD - 1; count++) {
-            // Pick the minimum distance vertex from the set of vertices
-            // not yet processed. u is always equal to dep in first
-            // iteration.
-            int u = minDistance(dist, cheminMin);
-            //System.out.println("u" + u);
+        // Parent array to store shortest
+        // path tree
+        int[] parents = new int[nVertices];
 
-            // Mark the picked vertex as processed
-            cheminMin[u] = true;
+        // The starting vertex does not
+        // have a parent
+        parents[startVertex] = NO_PARENT;
 
-            // Update dist value of the adjacent vertices of the
+        // Find shortest path for all
+        // vertices
+        for (int i = 1; i < nVertices; i++)
+        {
+
+            // Pick the minimum distance vertex
+            // from the set of vertices not yet
+            // processed. nearestVertex is
+            // always equal to startNode in
+            // first iteration.
+            int nearestVertex = -1;
+            int shortestDistance = Integer.MAX_VALUE;
+            for (int vertexIndex = 0;
+                 vertexIndex < nVertices;
+                 vertexIndex++)
+            {
+                if (!added[vertexIndex] &&
+                        shortestDistances[vertexIndex] <
+                                shortestDistance)
+                {
+                    nearestVertex = vertexIndex;
+                    shortestDistance = shortestDistances[vertexIndex];
+                }
+            }
+
+            // Mark the picked vertex as
+            // processed
+            added[nearestVertex] = true;
+
+            // Update dist value of the
+            // adjacent vertices of the
             // picked vertex.
-            for (int v = 0; v < NB_NOEUD; v++)
+            for (int vertexIndex = 0;
+                 vertexIndex < nVertices;
+                 vertexIndex++)
+            {
+                int edgeDistance = adjacencyMatrix[nearestVertex][vertexIndex];
 
-                // Update dist[v] only if is not in cheminMin, there is an
-                // edge from u to v, and total weight of path from dep to
-                // v through u is smaller than current value of dist[v]
-                if (!cheminMin[v] && graph[u][v] != 0 && dist[u] != Integer.MAX_VALUE && dist[u] + graph[u][v] < dist[v]){
-                    dist[v] = dist[u] + graph[u][v];
-                    System.out.println(dist[u] +" " + graph[u][v]);}
+                if (edgeDistance > 0
+                        && ((shortestDistance + edgeDistance) <
+                        shortestDistances[vertexIndex]))
+                {
+                    parents[vertexIndex] = nearestVertex;
+                    shortestDistances[vertexIndex] = shortestDistance +
+                            edgeDistance;
+                }
+            }
         }
 
-        // print the constructed distance array
-        //printSolution(dist);
-        System.out.println(Arrays.toString(cheminMin));
+        printSolution(startVertex, shortestDistances, parents);
     }
 
-    // Driver method
+    // A utility function to print
+    // the constructed distances
+    // array and shortest paths
+    private static void printSolution(int startVertex,
+                                      int[] distances,
+                                      int[] parents)
+    {
+        int nVertices = distances.length;
+        System.out.print("Vertex\t Distance\tPath");
+
+        for (int vertexIndex = 0;
+             vertexIndex < nVertices;
+             vertexIndex++)
+        {
+            if (vertexIndex != startVertex)
+            {
+                System.out.print("\n" + startVertex + " -> ");
+                System.out.print(vertexIndex + " \t\t ");
+                System.out.print(distances[vertexIndex] + "\t\t");
+                printPath(vertexIndex, parents);
+            }
+        }
+    }
+
+    // Function to print shortest path
+    // from source to currentVertex
+    // using parents array
+    private static void printPath(int currentVertex,
+                                  int[] parents)
+    {
+
+        // Base case : Source node has
+        // been processed
+        if (currentVertex == NO_PARENT)
+        {
+            return;
+        }
+        printPath(parents[currentVertex], parents);
+        System.out.print(currentVertex + " ");
+    }
+
+    // Driver Code
     public static void main(String[] args)
     {
-        /* Matrice de la map les noeuds sont bi-directionel */
-        int[][] graph = new int[][] { { 0, 5, 0, 0, 0, 1, 2, 0},
+        int[][] adjacencyMatrix = { { 0, 5, 0, 0, 0, 1, 2, 0},
                 { 5, 0, 1, 0, 0, 0, 3, 0},
                 { 0, 1, 0, 6, 0, 0, 0, 3},
                 { 0, 0, 6, 0, 12, 0, 0, 6},
@@ -97,9 +153,9 @@ class ShortestPath {
                 { 1, 0, 0, 0, 3, 0, 1, 0},
                 { 2, 3, 0, 0, 0, 1, 0, 10},
                 { 0, 0, 3, 6, 4, 0, 10, 0}};
-        ShortestPath t = new ShortestPath();
-        t.dijkstra(graph, 0, 0);
+
+        dijkstra(adjacencyMatrix, 0);
     }
 }
-// This code is contributed by Aakash Hasija
 
+// This code is contributed by Harikrishnan Rajan
