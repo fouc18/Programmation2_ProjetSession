336e11db81124ba7df2ba1f6e46d84244d7d8e25
Marc-Antoine Ricard <marcantoinericard@gmail.com>
2021-03-29 19:54:33 -0400

    Modifier Voiture pour qu'il fasse un peu plus de sens dans le projet

--- Code/Voiture.java
+++ Code/Voiture.java
@@ -1,26 +1,34 @@
+/* Nom: Voiture
+ * Version: 1.0
+ * Date: 03/28/2021
+ * Auteur: Membres de l'équipe 4
+ * Copyright 2021 Équipe 4
+ * */
 package Code;
 
+/* Cette class permet de representer le comportement d'une voiture */
 public class Voiture {
+    private int depart;
+    private int fin;
+    private int positionActuelle;
 
-        int year;
-
-        double speed;
-
-        public Voiture(int y,String m,double s){
-            this.year = y;
-
-            this.speed = s;
-        }
-
-        public double getSpeed(){
-            return this.speed;
-        }
-
-        public int getYear(){
-            return this.year;
-        }
-
-        public void accelerate(){
-            this.speed+=1;
-        }
+    public Voiture(){
+        this.depart = 'A';
+        this.fin = 'D';
+        this.positionActuelle = this.depart;
     }
+
+    public Voiture(char depart, char fin) {
+        this.depart = depart;
+        this.fin = fin;
+        this.positionActuelle = depart;
+    }
+
+    public boolean avancer(){
+        //super.getItineraire()
+        int[] chemin = {0,1,2,5};
+
+        return true;
+    }
+
+}
